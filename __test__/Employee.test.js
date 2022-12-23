d4e5df48a8db0353b79b902dc1bd62768f84a909
klgibsonjr <klgibsonjr@gmail.com>
2022-12-22 21:51:44 -0800

    passed first test for employee object

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,13 +1,11 @@
 const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
-  describe('createEmployee', () => {
+  describe('createTeam', () => {
     it('Should create an employee object', () => {
       const employee = new Employee();
 
       expect(typeof employee).toBe('object');
     });
-
-    it();
   });
 });
