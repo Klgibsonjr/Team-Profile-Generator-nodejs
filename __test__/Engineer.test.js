6ba72607bd81ea26390a8b98740cbcfa6245eb23
klgibsonjr <klgibsonjr@gmail.com>
2022-12-24 11:50:01 -0800

    completed engineer testing

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -8,7 +8,7 @@ describe('Engineer', () => {
       expect(typeof engineer).toBe('object');
     });
 
-    it('should get engineer github from constructor function', () => {
+    it('Should get engineer github from constructor function', () => {
       const testGithub = 'klgibsonjr';
       const engineer = new Engineer(
         'Ken',
@@ -20,6 +20,28 @@ describe('Engineer', () => {
       expect(engineer.github).toBe(testGithub);
     });
 
-    it('');
+    it('Should get engineer role from getRole()', () => {
+      const testRole = 'Engineer';
+      const engineer = new Engineer(
+        'Ken',
+        100,
+        'kgibson@test.com',
+        'klgibsonjr'
+      );
+
+      expect(engineer.getRole()).toBe(testRole);
+    });
+
+    it('Should get engineer github username from getGithub()', () => {
+      const testGithub = 'klgibsonjr';
+      const engineer = new Engineer(
+        'Ken',
+        100,
+        'kgibson@test.com',
+        'klgibsonjr'
+      );
+
+      expect(engineer.getGithub()).toBe(testGithub);
+    });
   });
 });
